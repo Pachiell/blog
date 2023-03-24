5629c8f1ab0b81e015ff233dbe740b400f0dfb5e
Pachiell <gundamms310@yahoo.co.jp>
2023-03-25 04:28:04 +0900

    エラーメッセージの表示　完了

--- post.php
+++ post.php
@@ -104,11 +104,13 @@
  <form action="post.php" method="post">
   <div class="mb-3">
      <label class="form-label">タイトル</label>
-     <input type="text" name="title" class="form-control">
+     <?php echo !empty($title_alert)? '<div class="alert alert-danger">'.$title_alert.'</div>': '' ?>
+     <input type="text" name="title" value="<?php echo $title; ?>" class="form-control">
   </div>
   <div class="mb-3">
      <label class="form-label">本文</label>
-     <textarea name="body" class="form-control" rows="10"></textarea>
+     <?php echo !empty($body_alert)? '<div class="alert alert-danger">'.$body_alert.'</div>': '' ?>
+     <textarea name="body" class="form-control" rows="10"><?php echo $body; ?></textarea>
   </div>
   <div class="mb-3">
      <button type="submit" class="btn btn-primary">投稿する</button>
