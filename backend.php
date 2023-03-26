f3f63f5f2a8aa49310c0c2301c1c024a7e776937
Pachiell <gundamms310@yahoo.co.jp>
2023-03-27 03:14:26 +0900

    投稿一覧の作成完了

--- backend.php
+++ backend.php
@@ -2,6 +2,12 @@
 
 <?php
   include 'lib/secure.php';
+  include 'lib/connect.php';
+  include 'lib/queryArticle.php';
+  include 'lib/article.php';
+
+  $queryArticle = new QueryArticle();
+  $articles = $queryArticle->findAll();
 ?>
 <!doctype html>
 <html lang="ja">
@@ -57,8 +63,38 @@
 <main class="container">
   <div class="row">
     <div class="col-md-12">
+    <h1>記事一覧</h1>
 
-      <p>本文がここに入ります。</p>
+<?php if ($articles): ?>
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>本文</th>
+            <th>作成日</th>
+            <th>更新日</th>
+            <th>編集</th>
+          </tr>
+        </thead>
+        <tbody>
+<?php foreach ($articles as $article): ?>
+          <tr>
+            <td><?php echo $article->getId() ?></td>
+            <td><?php echo $article->getTitle() ?></td>
+            <td><?php echo $article->getBody() ?></td>
+            <td><?php echo $article->getCreatedAt() ?></td>
+            <td><?php echo $article->getUpdatedAt() ?></td>
+            <td><a href="edit.php?id=<?php echo $article->getId() ?>" class="btn btn-success">編集</a></td>
+          </tr>
+<?php endforeach ?>
+        </tbody>
+      </table>
+<?php else: ?>
+      <div class="alert alert-info">
+        <p>記事はありません。</p>
+      </div>
+<?php endif ?>  
 
     </div>
 
